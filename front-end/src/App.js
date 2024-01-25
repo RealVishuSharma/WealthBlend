83e3ffb2f145c0227200e70056d137c16f8bddb3
RealVishuSharma <vishusharmaa2211@gmail.com>
2024-01-25 22:39:34 +0530

    Added BootStrap and Tailwind

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vishu Sharma
         </a>
       </header>
     </div>
